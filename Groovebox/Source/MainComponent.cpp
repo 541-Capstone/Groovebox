8844cbc8a15115a79b6a321b647c3171ec0cab3f
Dima <dmitrismith5@gmail.com>
2022-01-26 18:09:19 -0800

    Update gitignore

--- Groovebox/Source/MainComponent.cpp
+++ Groovebox/Source/MainComponent.cpp
@@ -3,9 +3,24 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+
+    addAndMakeVisible(playBtn);
+    addAndMakeVisible(stopBtn);
+    addAndMakeVisible(addTrackBtn);
+
+    addAndMakeVisible(statusLbl);
+    addAndMakeVisible(trackCountLbl);
+
+    startTimer(250);
+
+
+    playBtn.onClick = [this]() { play(); };
+    stopBtn.onClick = [this]() { stop(); };
+    addTrackBtn.onClick = [this]() {addTrack(); };
+
     // Make sure you set the size of the component after
     // you add any child components.
-    setSize (800, 600);
+    setSize (600, 300);
 
     // Some platforms require permissions to open input channels so request that here
     if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
@@ -19,12 +34,19 @@ MainComponent::MainComponent()
         // Specify the number of input and output channels that we want to open
         setAudioChannels (2, 2);
     }
+
+    //auto tracks = edit.trackCache;
+    //for(int i = 0; i < tracks.)
 }
 
 MainComponent::~MainComponent()
 {
     // This shuts down the audio device and clears the audio source.
     shutdownAudio();
+}
+
+void MainComponent::timerCallback() {
+    repaint();
 }
 
 //==============================================================================
@@ -48,6 +70,12 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
     // Right now we are not producing any data, in which case we need to clear the buffer
     // (to prevent the output of random noise)
     bufferToFill.clearActiveBufferRegion();
+
+    if (edit.getTransport().isPlaying()) {
+        //DBG("Time: " << edit.getCurrentPlaybackContext()->getPosition());
+        DBG("Time: " << edit.getTransport().getCurrentPosition());
+    }
+
 }
 
 void MainComponent::releaseResources()
@@ -65,6 +93,15 @@ void MainComponent::paint (juce::Graphics& g)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     // You can add your drawing code here!
+    statusLbl.setText((juce::String)edit.getTransport().getCurrentPosition(), juce::NotificationType::dontSendNotification);
+    trackCountLbl.setText((juce::String)edit.getTrackList().size(), juce::NotificationType::dontSendNotification);
+    juce::Array<tracktion_engine::Track*> trackList = edit.getTrackList().objects;
+    juce::String trackNames = "";
+    for (int i = 0; i < trackList.size(); i++) {
+        trackNames += trackList[i]->getName();
+        trackNames += '\n';
+    }
+    trackCountLbl.setText(trackNames, juce::NotificationType::dontSendNotification);
 }
 
 void MainComponent::resized()
@@ -72,4 +109,65 @@ void MainComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    juce::Rectangle<int> bounds = getLocalBounds();
+
+    juce::FlexBox buttonBox{ juce::FlexBox::Direction::row,
+                             juce::FlexBox::Wrap::noWrap,
+                             juce::FlexBox::AlignContent::stretch,
+                             juce::FlexBox::AlignItems::center,
+                             juce::FlexBox::JustifyContent::spaceAround };
+
+    juce::FlexBox textBox{ juce::FlexBox::Direction::column,
+                           juce::FlexBox::Wrap::noWrap,
+                           juce::FlexBox::AlignContent::flexStart,
+                           juce::FlexBox::AlignItems::flexStart,
+                           juce::FlexBox::JustifyContent::center
+    };
+
+    juce::FlexBox container{ juce::FlexBox::Direction::column,
+                             juce::FlexBox::Wrap::noWrap,
+                             juce::FlexBox::AlignContent::spaceAround,
+                             juce::FlexBox::AlignItems::center,
+                             juce::FlexBox::JustifyContent::center
+    };
+
+    buttonBox.items.add(juce::FlexItem(150, 150, playBtn));
+    buttonBox.items.add(juce::FlexItem(150, 150, stopBtn));
+    buttonBox.items.add(juce::FlexItem(150, 150, addTrackBtn));
+
+    textBox.items.add(juce::FlexItem(150, 100, statusLbl));
+    textBox.items.add(juce::FlexItem(150, 100, trackCountLbl));
+
+    container.items.add(buttonBox);
+    container.items.add(textBox);
+
+    container.performLayout(bounds);
+}
+
+void MainComponent::play() {
+    if (playState == PlayState::Stop) {
+        playState = PlayState::Play;
+        DBG("Play\n");
+        edit.getTransport().play(false);
+    }
+
+    auto track = tracktion_engine::getAllTracks(edit);
+}
+
+void MainComponent::stop() {
+    if (playState == PlayState::Play) {
+        playState = PlayState::Stop;
+    }
+    else if (playState == PlayState::Record) {
+        playState = PlayState::Stop;
+    }
+    DBG("Stop\n");
+
+    edit.getTransport().stop(false,false);
+    edit.getTransport().setCurrentPosition(0.0);
+
+}
+
+void MainComponent::addTrack() {
+    //edit.insertNewTrack(tracktion_engine::TrackInsertPoint::precedingTrackID, )
 }
--- Groovebox/Source/MainComponent.h
+++ Groovebox/Source/MainComponent.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <JuceHeader.h>
+//#include "Utilities.h"
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent  : public juce::AudioAppComponent, public juce::Timer
 {
 public:
     //==============================================================================
@@ -23,9 +24,46 @@ public:
     void paint (juce::Graphics& g) override;
     void resized() override;
 
+    void timerCallback() override;
+
+    enum class PlayState {
+        Play,
+        Stop,
+        Record
+    };
+
 private:
     //==============================================================================
     // Your private member variables go here...
+    tracktion_engine::Engine engine { ProjectInfo::projectName };
+
+    /*te::Edit::Options editOptions{ engine,
+        te::createEmptyEdit(engine),
+        te::ProjectItemID::createNewID(0),
+        te::Edit::EditRole::forEditing,
+        nullptr,
+        0 };*/
+
+
+
+    tracktion_engine::Edit edit{ engine,
+                                 tracktion_engine::createEmptyEdit(engine),
+                                 tracktion_engine::Edit::EditRole::forEditing,
+                   nullptr,
+                   0 };
+
+    juce::TextButton playBtn{ "Play" };
+    juce::TextButton stopBtn{ "Stop" };
+    juce::TextButton addTrackBtn{ "Add Track" };
+    juce::Label statusLbl{ "statusLbl", "Status" };
+    juce::Label trackCountLbl{ "trackCountLbl", "Track count" };
+
+    PlayState playState = PlayState::Stop;
+
+    void play();
+    void stop();
+    void record();
+    void addTrack();
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
